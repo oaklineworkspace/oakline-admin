143453f0e0cd4cac6c287cb79eab125a2efd27b1
oaklineworkspace <oaklineworkspace@gmail.com>
2025-10-17 01:46:06 -0700

    Delete pages directory

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import { AuthProvider } from '../contexts/AuthContext';
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
-  );
-}
