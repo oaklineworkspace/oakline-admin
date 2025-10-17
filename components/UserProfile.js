6a655aac2436bda17127c794f2cd58cb6304311b
oaklineworkspace <oaklineworkspace@gmail.com>
2025-10-17 02:33:08 -0700

    Add files via upload

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import styles from './UserProfile.module.css';
-
 export default function UserProfile({ user, onLogout }) {
   return (
-    <div className={styles.container}>
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
       <h2>Welcome, {user.email}!</h2>
-      <button onClick={onLogout} className={styles.logoutButton}>
+      <button onClick={onLogout} style={{ padding: '0.5rem 1rem', marginTop: '1rem' }}>
         Logout
       </button>
     </div>
-  );
+  )
 }
